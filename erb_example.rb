5160c9dbfb81cb45b39110d0eb72a77bdf1579fa
Brian Pham <phambrian@live.com>
2013-11-19 22:19:06 -0800

    hmwk section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,16 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+str = "This is a string"
+temp = ERB.new "The value of str is: <%= str %>"
+puts temp.result(binding)
+
+array = ['dog' => 'woof', 'cat' => 'meow', 'foo' => 'bar']
+array.concat(['cow' => 'moo'])
+atemp = ERB.new "The value of the array is: <%= array %>"
+puts atemp.result(binding)
+
+h = { 'key' => 'value', 'easy' => 'simple', 'hard' => 'complicated' }
+htemp = ERB.new "The value of the hash is: <%= h %>"
+puts htemp.result(binding)
